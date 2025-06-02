1d0e6b3c5506bd7f6e17d789106f0612e1ae9a2d
Gabriel Marins <gabrielmarins26@gmail.com>
2025-06-02 10:53:43 -0300

    Atualização do manifesto e do script de fundo para alterar o domínio da extensão de 'www.google.com' para 'example.com'. Ajustes na formatação de strings e mensagens de log para maior consistência.

--- src/background.js
+++ src/background.js
@@ -1,20 +1,19 @@
 chrome.action.onClicked.addListener((tab) => {
-  if (tab.url.includes('www.google.com')) {
+  if (tab.url.includes("example.com")) {
     chrome.tabs.sendMessage(tab.id, {
-      action: "toggle_overlay"
+      action: "toggle_overlay",
     });
   } else {
-    console.log('Extensão só funciona no domínio configurado');
+    console.log("Extensão só funciona no domínio configurado");
   }
 });
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.action === "reload_extension") {
     chrome.runtime.reload();
-  }
-  else if (message.action === "get_user_email") {
+  } else if (message.action === "get_user_email") {
     chrome.identity.getProfileUserInfo((userInfo) => {
-      sendResponse({ email: userInfo.email || 'user@local.com' });
+      sendResponse({ email: userInfo.email || "user@local.com" });
     });
     return true; // Mantém o canal aberto para resposta assíncrona
   }
